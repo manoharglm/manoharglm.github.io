eab240dcd7e496cb982d667774b333a1a369df27
manoharglm <manohargunduboina@gmail.com>
2025-02-19 20:18:55 +0530

    Add initial project configuration and update homepage layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true, // Since Next.js Image Optimization doesn't work with static export
+  },
+  basePath: "/manoharglm.github.io",
+  assetPrefix: "/manoharglm.github.io/",
+};
 
-module.exports = nextConfig
+export default nextConfig;
