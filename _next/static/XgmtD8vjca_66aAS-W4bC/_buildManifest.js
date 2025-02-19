c8cd7fd4cc71bd7770d3b1c3dc2dfef3c8023ee9
manoharglm <manohargunduboina@gmail.com>
2025-02-19 21:37:44 +0530

    Updates

---
+++ _next/static/XgmtD8vjca_66aAS-W4bC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-1b1236a75308915e.js"],"/_error":["static/chunks/pages/_error-73a1e808a3155de4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
