f6ac53c3d1099e2bf8768fb21b100b5c9cf7e054
manoharglm <34521899+manoharglm@users.noreply.github.com>
2026-03-28 18:49:56 +0000

    Deploying to gh-pages from @ manoharglm/manoharglm.github.io@7f60b5730766229486f191ce526cd05942a8c397 🚀

--- _next/static/J9sKTLFxzDxHF5YH-XD7v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-77ad8c6fd1dd2096.js"],"/_error":["static/chunks/pages/_error-73a1e808a3155de4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
